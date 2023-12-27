044979e93cdd2a6f9e621a37a9ec5a5d4740d802
Quen <grrkekisbest@gmail.com>
2023-12-27 00:54:31 -0500

    Changed it so that shell will get every .mp4 file and then compress them down

--- File_srink.sh
+++ File_srink.sh
@@ -5,8 +5,8 @@ for eachfile in $List_files
 do
 	Array+=($eachfile)
 done
-
+Array="${Array[@]:1}"
 for object in "${Array[@]}"
 do
-	ffmpeg -i $object -b 1000k -vcodec wmv2 -acodec wmav2 -crf 19 -filter:v fps=fps=24 output.wmv
+	ffmpeg -i $object -b 1000k -vcodec wmv2 -acodec wmav2 -crf 19 -filter:v fps=fps=24 ${object::-3}mp4
 done
